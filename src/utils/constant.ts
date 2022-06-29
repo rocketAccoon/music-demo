95126e6d05cb05529959db68c3c1f78cc80163b4
xiongzf <xiongzhongfu@indexvc.com>
2022-06-29 09:38:55 +0800

    新增

---
+++ src/utils/constant.ts
@@ -0,0 +1,13 @@
+export const baseUrl: string = process.env.VUE_APP_BASE_URL
+
+// 歌手区域
+export const SINGER_AREAS: Array<SingerSelectArgs> = [
+  {label: '全部', value: -1},
+  {label: '华语', value: 7},
+  {label: '欧美', value: 96},
+  {label: '日本', value: 8},
+  {label: '其他', value: 0}
+]
+// 歌手类型
+// 歌手首字母
+
